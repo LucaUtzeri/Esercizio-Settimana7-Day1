714ebb05b6e0a20659c396b6ef8b5c4eb9aed595
Luca Utzeri <lutzeri@libero.it>
2023-11-06 16:53:54 +0100

    first commit/incomplete

---
+++ script.js
@@ -0,0 +1,8 @@
+class Utente {
+  constructor(firstName, lastName, age, location) {
+    this.name = firstName;
+    this.surname = lastName;
+    this.age = age;
+    this.location = location;
+  }
+}
